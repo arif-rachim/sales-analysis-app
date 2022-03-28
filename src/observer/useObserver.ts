52a4badf58f8f609f6dce3236be8642ddc0a5d55
Marsa Emreef <marsa.emreef@gmail.com>
2022-03-28 15:36:28 +0400

    Fix : Refactor useObserver

--- src/observer/useObserver.ts
+++ src/observer/useObserver.ts
@@ -1,28 +1,36 @@
 import {MutableRefObject, useMemo, useRef} from "react";
 
-export type Observer<S> =
-    MutableRefObject<S>
-    & { addListener: (listener: (value: S) => void) => () => void };
+type UnRegisterCallback = () => void;
+type OnListener<S> = (value: S) => void;
+
+export interface Observer<S> extends MutableRefObject<S> {
+    addListener: (listener: OnListener<S>) => UnRegisterCallback
+}
 
 function isFunction(functionToCheck: any) {
     return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
 }
-const noOp = () => () => {};
 
+const noOp = () => () => {
+};
 
-export function useObserver<S>(initialValue: (S | (() => S))): [Observer<S>, (value: ((value: S) => S) | S) => void] {
+type Dispatch<A> = (value: A) => void;
+type SetObserverAction<S> = S | ((prevState: S) => S);
+type Initialization<S> = S | (() => S)
+
+export function useObserver<S>(initialValue: Initialization<S>): [Observer<S>, Dispatch<SetObserverAction<S>>] {
 
     const defaultValueRef = useRef(initialValue);
     return useMemo(() => {
         let listeners: Function[] = [];
         let valueIsInitialized = false;
         const $value: Observer<S> = {
-            current : {} as S,
-            addListener:noOp
+            current: {} as S,
+            addListener: noOp
         };
 
         const currentValue = isFunction(defaultValueRef.current) ? (defaultValueRef.current as Function).call(null) : defaultValueRef.current;
-        
+
         function setValue(callbackOrValue: S | ((oldValue: S) => S)) {
             const oldVal = valueIsInitialized ? $value.current : defaultValueRef.current;
             let newVal: S | null = null;
@@ -44,7 +52,7 @@ export function useObserver<S>(initialValue: (S | (() => S))): [Observer<S>, (va
             })
         }
 
-        function addListener(listener: (value: S) => void) {
+        function addListener(listener: OnListener<S>) {
             listeners.push(listener);
             return () => {
                 listeners.splice(listeners.indexOf(listener), 1);
